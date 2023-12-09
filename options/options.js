00d52e9a8eb74a5d09373e6b165939a7b3df52d7
Crowdbotics <team@crowdbotics.com>
2023-12-09 19:50:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "emedia-tech-44937",
+  url: "https://emedia-tech-44937.botics.co",
+  api: "https://emedia-tech-44937.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
